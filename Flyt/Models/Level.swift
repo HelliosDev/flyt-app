20dbb21f8e4e0b114da9f322ea7fcefd3baadff4
HelliosDev <wiky.pedia@yahoo.com>
2020-05-14 11:14:05 +0700

    Initial Commit

---
+++ Flyt/Models/Level.swift
@@ -0,0 +1,14 @@
+//
+//  Level.swift
+//  FootworkApp
+//
+//  Created by Wendy Kurniawan on 12/05/20.
+//  Copyright © 2020 Wendy Kurniawan. All rights reserved.
+//
+
+import Foundation
+struct Level {
+    var name: String
+    var time: Int
+    var description: String
+}
